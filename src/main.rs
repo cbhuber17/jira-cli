b01b01ca9a4895eac17f62fe0b2e3812f2c17ce6
Colin Huber <cbhuber@gmail.com>
2024-03-17 18:15:20 -0600

    feat: App implementation

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,43 @@ mod ui;
 mod io_utils;
 mod navigator;
 
+use std::rc::Rc;
+use db::*;
+use io_utils::*;
+use navigator::*;
+
+
 fn main() {
-    println!("JIRA CLI");
+    let db = Rc::new(JiraDatabase::new("./data/db.json".to_owned()));
+    let mut navigator = Navigator::new(Rc::clone(&db));
+    
+    loop {
+        clearscreen::clear().unwrap();
+
+        if let Some(page) = navigator.get_current_page() {
+            if let Err(error) = page.draw_page() {
+                println!("Error rendering page: {}\nPress any key to continue...", error);
+                wait_for_key_press();
+            };
+
+            let user_input = get_user_input();
+
+            match page.handle_input(user_input.trim()) {
+                Err(error) => {
+                    println!("Error getting user input: {}\nPress any key to continue...", error);
+                    wait_for_key_press();
+                }
+                Ok(action) => {
+                    if let Some(action) = action {
+                        if let Err(error) = navigator.handle_action(action) {
+                            println!("Error handling processing user input: {}\nPress any key to continue...", error);
+                            wait_for_key_press();
+                        }
+                    }
+                }         
+            }
+        } else {
+            break;
+        }
+    }
 }
